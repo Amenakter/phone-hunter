c3454c2e97004490506bf7e9c8ca30c1efa23f8a
Amenakter <amenakter27@gmail.com>
2022-03-01 11:45:01 +0600

    details part

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const loadData =async () => {
 
 // display data
 const displayPhone =  phones => {
-    
-    
+     
+    const phone20 = phones.slice(0,20)
     const phoneContainer = document.getElementById('phones')
     phoneContainer.textContent = '';
 
-    phones.forEach(phone => {
-            // console.log(phone)
+    phone20.forEach(phone => {
+            // console.log(phones.slice(0,20))
             const phonesDiv = document.createElement('div');
             phonesDiv.classList.add('col');
             phonesDiv.innerHTML = `<div class="card h-100">
@@ -40,3 +40,42 @@ const displayPhone =  phones => {
         
 } 
 
+const phoneDetails = async id => {
+    // console.log(id);
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    console.log(url);
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = details => {
+    console.log(details);
+    const detailContainer = document.getElementById('details');
+    detailContainer.textContent=''
+    const card = document.createElement('div')
+    card.classList.add('card');
+    
+    card.innerHTML = `
+                      
+                       
+                <img src="${details.image}"  class="img card-img-top w-25 align-self-center mt-5">
+                <div class="card-body">
+                  <h5 class="card-title">${details.name}</h5>
+                  <h5 class="card-title">${details.releaseDate? details.releaseDate:'Release Date is Not Found'}</h5>
+                 </div>
+                 <h6>Main Features:</h6>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Storage : ${details.mainFeatures.storage}</li>
+                  <li class="list-group-item">ChipSet : ${details.mainFeatures.chipSet}</li>
+                  <li class="list-group-item">displaySize : ${details.mainFeatures.displaySize}</li>
+                  <li class="list-group-item">Memory : ${details.mainFeatures.memory}</li>
+                </ul>
+                <div class="card-body">
+                <a onclick="others('${details.slug}')" class="btn btn-primary">Others info</a>
+                </div>
+
+
+                     `
+    detailContainer.appendChild(card)
+}
